cf7ba5baab50f73dd6bab4e9ac9a1f2678700e6d
ttnhuy313 <39652384+ttnhuy313@users.noreply.github.com>
2023-04-27 00:33:22 +0700

    complete section bar

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FullNavigationBar from "./FullNavigationBar";
 import SectionBar from "./SectionBar";
+import Home from "./Home";
 const DashBoard = () => {
     return (
         <div>
             <FullNavigationBar />
             <SectionBar />
+            <Home />
         </div>
     )
 }
--- src/components/dashboard/SectionBar.js
+++ src/components/dashboard/SectionBar.js
@@ -3,8 +3,25 @@ import React from "react";
 const SectionBar = () => {
     return (
         <div>
-            <div className="flex items-center justify-center w-full h-96 bg-gray-700">
-                
+            <div className="flex items-center justify-start w-full h-11 bg-gray-700 space-x-10 px-8">
+                <div className="text-sm font-medium text-teal-500">
+                    Home
+                </div>
+                <a href="/all-files" className="text-sm font-medium text-gray-100 hover:text-teal-500">
+                    All Files
+                </a>
+                <a href="/topics" className="text-sm font-medium text-gray-100 hover:text-teal-500">
+                    Your Topics
+                </a>
+                <a href="/vocabs" className="text-sm font-medium text-gray-100 hover:text-teal-500">
+                    Vocab Hub
+                </a>
+                <a href="/reminders" className="text-sm font-medium text-gray-100 hover:text-teal-500">
+                    Reminder Area
+                </a>
+                <a href="/faq" className="text-sm font-medium text-gray-100 hover:text-teal-500">
+                    Support
+                </a>
             </div>
         </div>
     );
