8559a9ed17bd8d5f27fef586bb262fcaa2c1e3ca
ttnhathuy313 <89716952+ttnhathuy313@users.noreply.github.com>
2023-04-26 15:32:52 +0000

    set up section bar

---
+++ src/components/dashboard/SectionBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SectionBar = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-center w-full h-96 bg-gray-700">
+                
+            </div>
+        </div>
+    );
+}
+
+export default SectionBar;
