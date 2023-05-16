d2960f02151be2b390aab63f91b7f3ca553b673f
ttnhuy313 <39652384+ttnhuy313@users.noreply.github.com>
2023-05-16 18:06:16 +0700

    test2

--- src/components/topics/TopicsBody.js
+++ src/components/topics/TopicsBody.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Progress } from "flowbite-react";
 import "./TopicsBody.css"
 
 const topicData = [
