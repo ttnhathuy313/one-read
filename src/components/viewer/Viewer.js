f331d4820b7b3fc9f9e982810197de0ad4ceb6cd
ttnhuy313 <39652384+ttnhuy313@users.noreply.github.com>
2023-05-23 23:16:25 +0700

    messing up with pdf files

--- src/components/viewer/Viewer.js
+++ src/components/viewer/Viewer.js
@@ -5,20 +5,28 @@ import * as pdfjs from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { TextLayerBuilder } from 'pdfjs-dist/web/pdf_viewer';
+import { GlobalWorkerOptions } from 'pdfjs-dist';
 
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+GlobalWorkerOptions.workerSrc = pdfjsWorker;
+let renderTask = null
+console.log(typeof TextLayerBuilder)
 
 const Viewer = () => {
 
     const [pdfDoc, setPdfDoc] = useState(null);
     const [pageNum, setPageNum] = useState(1);
     const canvasRef = useRef();
+    const textLayerRef = useRef();
     const numPages = pdfDoc ? pdfDoc._pdfInfo.numPages : 0;
 
     const renderPage = useCallback(async ({ pdfDoc, pageNum, scale }) => {
+        if (renderTask) {
+            await renderTask.cancel();
+        }
         const page = await pdfDoc.getPage(pageNum);
         const viewport = page.getViewport({ scale: scale });
-        const canvas = canvasRef.current;
+        var canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         canvas.height = viewport.height;
         canvas.width = viewport.width;
@@ -27,36 +35,59 @@ const Viewer = () => {
             viewport: viewport
         }
         // issue: what happens for the first page?
-        page.render(renderContext);
+        renderTask = page.render(renderContext);
+
+        textLayer = document.querySelector(".textLayer");
+        const textContent = await page.getTextContent();
+        const textLayer = textLayerRef.current;
+        const textLayerBuilder = new TextLayerBuilder({
+            textLayerDiv: textLayer,
+            pageIndex: pageNum,
+            viewport: viewport
+        });
+        pdfjs.renderTextLayer({
+            textContent: textContent,
+            container: textLayer,
+            viewport: viewport,
+            textDivs: []
+        });
+
+        textLayer.style.position = 'absolute';
+        textLayer.style.left = canvas.offsetLeft + 'px';
+        textLayer.style.top = canvas.offsetTop + 'px';
+        textLayer.style.height = canvas.offsetHeight + 'px';
+        textLayer.style.width = canvas.offsetWidth + 'px';
+        textLayerBuilder.setTextContentSource(textContent);
+        await textLayerBuilder.render(viewport);
+        console.log(textLayerBuilder)
     }, []
     )
 
 
     useEffect(() => {
         async function loadPdf() {
-            const gsReference = ref(storage, 'gs://one-read-72fb0.appspot.com/test_files/polymer-lgv2.pdf');
-            let url = null;
-            await getDownloadURL(gsReference).then((url_) => {
-                url = url_;
-            });
-            const loadedPdf = await pdfjs.getDocument(url).promise;
-
-            setPdfDoc(loadedPdf);
-            renderPage({ pdfDoc: loadedPdf, pageNum, scale: 1 })
+            if (pdfDoc === null) {
+                const gsReference = ref(storage, 'gs://one-read-72fb0.appspot.com/test_files/polymer-lgv2.pdf');
+                let url = null;
+                await getDownloadURL(gsReference).then((url_) => {
+                    url = url_;
+                });
+                const loadedPdf = await pdfjs.getDocument(url).promise;
+                setPdfDoc(loadedPdf);
+            }
+            renderPage({ pdfDoc: pdfDoc, pageNum, scale: 1 })
         }
 
         loadPdf();
-    }, [])
+    }, [pageNum, renderPage, pdfDoc])
 
     const increasePageNum = () => {
         if (pageNum < numPages) {
-            renderPage({ pdfDoc, pageNum: pageNum + 1, scale: 1 });
             setPageNum(pageNum + 1);
         }
     }
     const decreasePageNum = () => {
         if (pageNum > 1) {
-            renderPage({ pdfDoc, pageNum: pageNum - 1, scale: 1 });
             setPageNum(pageNum - 1);
         }
     }
@@ -76,6 +107,7 @@ const Viewer = () => {
             <div className="canvas-container">
                 <div>
                     <canvas ref={canvasRef} />
+                    <div ref={textLayerRef} className="textLayer" id="text-layer textLayer"></div>
                 </div>
             </div>
         </div>
